3aab1384eba8f6b8e2971d8d4c1b4ccd515630ce
kartik2406 <kartikmalik91@gmail.com>
2018-08-29 20:20:14 +0530

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>
+      I am Kartik Malik, UI Developer by profession, Full stack developer from
+      heart. I like to try different technologies, build small full stack apps.
+    </p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
